44c537e310dbae95b51e49f133026a7ee8718427
ManozGial <emmanuel.gialitakis@gmail.com>
2018-04-07 15:05:58 +0300

    Loop Leftovers

--- test1.c
+++ test1.c
@@ -146,9 +146,25 @@ for(int i=0;i<N;i++)
 			float den_1 = mVec[i]*nVec[i];
 			float den = den_0/den_1;
 			FVec[i] = num/(den+0.01);
+			maxF = FVec[i]>maxF?FVec[i]:maxF;
 */
 			
 		}
+		for (int i=0; i<extra; i++){
+			float num_0 = LVec[i]+RVec[i];
+			float num_1 = mVec[i]*(mVec[i]-1.0)/2.0;
+			float num_2 = nVec[i]*(nVec[i]-1.0)/2.0;
+			float num = num_0/(num_1+num_2);
+			
+			float den_0 = CVec[i]-LVec[i]-RVec[i];
+			float den_1 = mVec[i]*nVec[i];
+			float den = den_0/den_1;
+			FVec[i] = num/(den+0.01);
+			maxF = FVec[i]>maxF?FVec[i]:maxF;
+			
+
+		}
+
 /*******************************************************************************************/
 
 			double time1=gettime();
